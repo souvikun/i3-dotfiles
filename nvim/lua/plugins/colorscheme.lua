c7b06ab67bba8bdfaa5517436ca3d12a2500ce88
Souvik Das <168964881+souvikun@users.noreply.github.com>
2025-02-10 17:26:17 +0530

    Add files via upload

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true, -- Enable transparency
+    },
+  },
+}
